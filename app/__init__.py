044dd2c041aa513329c5a25372d00433d407c178
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-15 13:42:47 +0330

    fix a type hint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.views.main import landing
 
 
-def create_app() -> "Flask App":
+def create_app() -> Flask:
     app = Flask(__name__)
     register_blueprints(app)
     return app
