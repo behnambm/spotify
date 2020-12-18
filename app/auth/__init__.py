6115e4751865c1d12c292b00fa21d743e6fb4ec5
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-18 17:09:12 +0330

    create spoitfy oauth remote-app object

---
+++ app/auth/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import session
+from flask_oauthlib.client import OAuth
+
+
+oauth = OAuth()
+
+spotify = oauth.remote_app(
+    'spotify',
+    consumer_key=os.getenv('SPOTIFY_CONSUMER_KEY'),
+    consumer_secret=os.getenv('SPOTIFY_CONSUMER_SECRET'),
+    base_url='https://api.spotify.com/',
+    request_token_url=None,
+    authorize_url='https://accounts.spotify.com/authorize',
+    access_token_url='https://accounts.spotify.com/api/token',
+    request_token_params={'scope': 'user-read-email'}
+)
+
+
+@spotify.tokengetter
+def get_spotify_access_token():
+    if 'access_token' in session:
+        return session.get('access_token')
