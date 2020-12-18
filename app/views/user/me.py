c742eb570703acd5170bb4809f600b27f4c7c56c
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-18 17:14:05 +0330

    add /me/ endpoint to get the users info

---
+++ app/views/user/me.py
@@ -0,0 +1,16 @@
+"""
+Users in this view can access their personal info
+"""
+from flask import Blueprint, jsonify, redirect, url_for
+from app.auth import spotify
+
+me_bp = Blueprint('me', __name__)
+
+
+@me_bp.route('/me/')
+def my_page():
+    me = spotify.get('me')
+    if 'error' in me.data:
+        if "The access token expired" in me.data.get('error').get('message'):
+            return redirect(url_for('auth.auth_user'))
+    return jsonify(me.data)
