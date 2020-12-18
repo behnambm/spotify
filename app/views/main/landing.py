db0ea8dd1f416dbef6e05f53e23fb4e6bc22d694
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-18 17:14:43 +0330

    add redirect to /me/ if user is already authorized

--- app/views/main/landing.py
+++ app/views/main/landing.py
@@ -2,13 +2,15 @@
 This view is for landing_bp page
 landing_bp page is the first page that user will see when opens the root URL
 """
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, session, redirect, url_for
 
-landing_bp = Blueprint('landing_bp', __name__)
+landing_bp = Blueprint('landing', __name__)
 
 
 @landing_bp.route('/')
 @landing_bp.route('/home/')
 @landing_bp.route('/index/')
-def landing_bp_page():
+def landing_page():
+    if 'access_token' in session:
+        return redirect(url_for('me.my_page'))
     return render_template('main/landing.html')
