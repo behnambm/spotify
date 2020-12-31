7808821da177f53641f0b6f367824bdea61657b2
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-12-31 20:53:50 +0330

    add /me/share/ back-end code

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from app.views.main import landing
 from app.views.spotify import authorize
-from app.views.user import me, top_tracks
+from app.views.user import me, top_tracks, share
 from app.auth import oauth
 from app.errors import errors_bp
-from app.models import db
-from app.models.playlist import PlaylistModel
 import os
 
 
@@ -24,3 +22,4 @@ def register_blueprints(app: Flask) -> None:
     app.register_blueprint(me.me_bp)
     app.register_blueprint(errors_bp)
     app.register_blueprint(top_tracks.top_tracks_bp)
+    app.register_blueprint(share.share_bp)
---
+++ app/views/user/share.py
@@ -0,0 +1,39 @@
+"""
+This view is to save the tracks that user wants to share
+"""
+from flask import Blueprint, session, request
+from app.utils import login_required
+import json
+from app.models import PlaylistModel, TracksModel
+
+
+share_bp = Blueprint('share', __name__, url_prefix='/me/')
+
+
+@share_bp.route('/share/', methods=['POST'])
+@login_required
+def share_tracks():
+    user_uris = request.form.get('uris')
+    if user_uris is None:
+        return 'bad request', 400
+
+    try:
+        user_uris = json.loads(user_uris)
+    except:
+        return 'bad request', 400
+
+    if session.get('playlist_data') is None:
+        return 'server error', 500
+
+    new_playlist = PlaylistModel(
+        playlist_name=session.get('display_name'),
+        owner_user_id=session.get('user_id')
+    )
+
+    for uri in user_uris:
+        new_playlist.tracks.append(TracksModel.find_by_uri(uri))
+
+    new_playlist.save_to_db()
+    print(new_playlist.tracks)
+
+    return 'this will change', 201
