1c9924ee8928844516dd5c2b8a279490476e906a
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-01-05 19:43:16 +0330

    use UUID in Playlist Model instead of incremental integer

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,29 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 from typing import Union
 import time
+from uuid import uuid4
 
 
 db = SQLAlchemy()
 
 association_table = db.Table(
     'spotify_association',
-    db.Column('playlist_id', db.Integer, db.ForeignKey('spotify_playlists.id')),
+    db.Column('playlist_id', db.String, db.ForeignKey('spotify_playlists.id')),
     db.Column('track_id', db.Integer, db.ForeignKey('spotify_tracks.id'))
 )
 
 
 class PlaylistModel(db.Model):
     __tablename__ = 'spotify_playlists'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     playlist_name = db.Column(db.String, nullable=False)
     import_count = db.Column(db.Integer, nullable=False, default=0)
     owner_user_id = db.Column(db.String)
     created_at = db.Column(db.Float, default=time.time)
     tracks = db.relationship('TracksModel', secondary=association_table)
 
+    def __init__(self, **kwargs) -> None:
+        self.id = uuid4().hex
+        super().__init__(**kwargs)
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def find_by_user_id(cls, user_id: str) -> Union['PlaylistModel', 'False']:
+        return cls.query.filter_by(owner_user_id=user_id).first() or False
+
+    @classmethod
+    def find_by_id_or_404(cls, playlist_id: str) -> Union['PlaylistModel', None]:
+        return cls.query.filter_by(id=playlist_id).first_or_404()
 
 
 class TracksModel(db.Model):
