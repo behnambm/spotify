1f8d86426badcfe48f8897691ce70913dcd3ddb3
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-01-10 18:41:44 +0330

    add support for editing playlist's name (front-end)

--- app/static/js/playlist.js
+++ app/static/js/playlist.js
@@ -1,13 +1,36 @@
-$(document).ready(
-    $('#delete-modal .btn-success').click((e)=>{
+$(document).ready(()=> {
+    $('#delete-modal .btn-success').click((e) => {
         $.ajax({
             url: '/playlist/delete',
             type: 'DELETE',
             statusCode: {
-                200: (resp)=>{
+                200: (resp) => {
                     window.location.replace(resp);
                 }
             }
+        });
+    })
+
+    // populate playlist edit input
+    $('#edit-name-modal').on('shown.bs.modal', (e) => {
+        let playlistCurrentName = $('#playlist-display-name').text();
+        $('#edit-name-input').val(playlistCurrentName);
+    });
+
+    // send ajax to change the playlist's name
+    $('#button-save-edit-name').click((e)=>{
+        let playlistCurrentName = $('#edit-name-input').val();
+        $.ajax({
+            url: window.location.pathname.split('/')[2] + '/change-name',
+            type: 'PUT',
+            data: {'new_name': playlistCurrentName},
+            success: (resp) => {
+                $('#playlist-display-name').text(resp);
+                $('#edit-name-modal').modal('hide');
+            },
+            fail: (resp) => {
+                alert('something went wrong!')
+            }
         })
     })
-);
+});
