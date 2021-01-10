70815f25d86ee6e26418310f87ec80dd143785db
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-01-10 18:40:45 +0330

    add view for changing playlists name

--- app/views/playlist/__init__.py
+++ app/views/playlist/__init__.py
@@ -2,7 +2,7 @@
 This view is going to show playlists info
 endpoint will be like this: /playlist/<PLAYLIST_ID>
 """
-from flask import Blueprint, render_template, session, abort, url_for
+from flask import Blueprint, render_template, session, abort, url_for, request
 from app.models import PlaylistModel
 from app.schema.playlist import PlaylistSchema
 from app.utils import login_required
@@ -40,3 +40,17 @@ def delete():
 
     user_playlist.delete_from_db()
     return url_for('me.my_page'), 200
+
+
+@playlist_bp.route('<playlist_id>/change-name', methods=['PUT'])
+@login_required
+def change_display_name(playlist_id):
+    playlist = PlaylistModel.find_by_id_or_404(playlist_id)
+
+    if not playlist.owner_user_id == session.get('user_id'):
+        abort(401)
+
+    playlist.playlist_name = request.form.get('new_name')
+    playlist.save_to_db()
+
+    return str(playlist.playlist_name), 200
