6fbd8b18f74de3051c8a6b30a60b532c3b31094f
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-01-15 11:58:02 +0330

    add pagination to main page

--- app/views/main/landing.py
+++ app/views/main/landing.py
@@ -2,17 +2,21 @@
 This view is for landing_bp page
 landing_bp page is the first page that user will see when opens the root URL
 """
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from app.models import PlaylistModel
 
 landing_bp = Blueprint('landing', __name__)
 
 
 @landing_bp.route('/')
-@landing_bp.route('/home/')
-@landing_bp.route('/index/')
 def landing_page():
+    page = request.args.get('page', 1, type=int)
+    playlists = PlaylistModel.query.order_by(PlaylistModel.created_at.desc()).paginate(
+        page=page, per_page=8, error_out=False
+    )
+
     return render_template(
         'home.html',
-        playlists=PlaylistModel.query.all(),
+        playlists=playlists.items,
+        pagination=playlists  # this is a pagination object
     )
