d4af99a7b18c2fd46e4ecbc2534121d5c714d7e9
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-01-17 16:16:38 +0330

    handle token expiration while importing playlist

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -2,11 +2,7 @@ from flask import Blueprint, redirect, url_for, session, abort, flash
 from flask_oauthlib.client import OAuthException
 from urllib.error import URLError
 from app.utils.errors import Unauthorized, TokenExpired
-from app.auth import spotify
-from app.utils import generate_basic_authorization_value
-from os import getenv
-from requests import post
-import json
+from app.utils import get_new_access_token
 
 errors_bp = Blueprint('errors', __name__)
 
@@ -39,33 +35,13 @@ def handle_spotify_unauthorized_error(e):
 @errors_bp.app_errorhandler(TokenExpired)
 def refresh_expired_token(e):
     """
-    This will handle the getting new access_token.
-    sends a POST request to `spotify.access_token_url` and gets a new token
-    using refresh token
+    This will handle the getting new access_token and redirecting to source endpoint
     """
-    data = {
-        'grant_type': 'refresh_token',
-        'refresh_token': session.get('refresh_token')
-    }
-    authorization_value = generate_basic_authorization_value(
-        consumer_id=getenv('SPOTIFY_CONSUMER_KEY'),
-        consumer_secret=getenv('SPOTIFY_CONSUMER_SECRET')
-    )
-    header = {
-        'Authorization': 'Basic ' + authorization_value.decode(),
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    new_token = get_new_access_token()
+    if new_token is None:
+        abort(500)
 
-    # use `requests` module to send the request
-    resp = post(
-        spotify.access_token_url,
-        data=data,
-        headers=header
-    )
-
-    if resp.status_code == 200:
-        data = json.loads(resp.text)
-        session['access_token'] = data.get('access_token')
-        # after getting get new token we need to redirect the user to the endpoint that
-        # the token_expired` error was initiated
-        return redirect(url_for(e.next_url))
+    session['access_token'] = new_token
+    # after getting get new token we need to redirect the user to the endpoint that
+    # the token_expired` error was initiated
+    return redirect(url_for(e.next_url))
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,10 +1,12 @@
-from flask import session, flash, redirect, url_for
+from flask import session, flash, redirect, url_for, abort
 from functools import wraps
 from base64 import b64encode
 from requests import post
 import json
 from typing import Union
 from app.utils.errors import TokenExpired
+from os import getenv
+from app.auth import spotify
 
 
 def login_required(f):
@@ -95,13 +97,72 @@ def create_playlist(name: str) -> Union[None, dict]:
         return json.loads(resp.text).get('id')
 
     if resp.status_code == 401:
-        # todo: handle token expiration
-        # return create_playlist(name=name)
-        pass
+        save_new_token_to_session()
+        return create_playlist(name=name)
 
     return None
 
 
-def add_tracks_to_playlist(track_uris: list, playlist_id):
-    pass
-    # POST https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+def add_tracks_to_playlist(track_uris: list, playlist_id) -> None:
+    """
+    This function will send a POST to https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+    to add items to new playlist the request contains list of track uris
+    :param track_uris: a list of uris
+    :param playlist_id: ID for new playlist
+    """
+    header = {
+        'Authorization': 'Bearer ' + str(session.get('access_token')),
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'uris': track_uris
+    }
+    try:
+        resp = post(
+            url=f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+            headers=header,
+            data=json.dumps(data)
+        )
+    except Exception as e:
+        # todo: add logging
+        abort(500)
+
+    if resp.status_code != 201:
+        # todo: add loggin
+        abort(500)
+
+
+def get_new_access_token():
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': session.get('refresh_token')
+    }
+    authorization_value = generate_basic_authorization_value(
+        consumer_id=getenv('SPOTIFY_CONSUMER_KEY'),
+        consumer_secret=getenv('SPOTIFY_CONSUMER_SECRET')
+    )
+    header = {
+        'Authorization': 'Basic ' + authorization_value.decode(),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    # use `requests` module to send the request
+    resp = post(
+        spotify.access_token_url,
+        data=data,
+        headers=header
+    )
+
+    if resp.status_code == 200:
+        data = json.loads(resp.text)
+        return data.get('access_token')
+    return None
+
+
+def save_new_token_to_session():
+    new_token = get_new_access_token()
+    if new_token is None:
+        abort(500)
+    session['access_token'] = new_token
+    return True
